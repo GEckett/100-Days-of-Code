6e7273b02bc857607a4963ad10710670b0341f25
GEckett <grant.eckett@gmail.com>
2023-06-30 12:12:22 +0100

    Create 100DoC-Day 6.py

---
+++ 100DoC-Day 6.py
@@ -0,0 +1,16 @@
+#Defining and Calling functions
+
+#print("Hello")
+#num_char = len("Hello")
+#print(num_char)
+
+#def my_function():
+#    print("Hello")
+#    print("Bye")
+#my_function()
+
+#While loop
+
+#while condition > 0:
+#    jump()
+#    condition -= 1
